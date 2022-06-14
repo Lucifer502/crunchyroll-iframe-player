28237b6a1fd5b8cd5c6e995ebbfdf743721fdcce
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-06-13 23:50:46 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,6 +1,6 @@
 window.addEventListener('message', async e => {
 
-  console.log(e)
+  console.log(e.data)
 
   let video_config_media = e.data.video_config_media;
   let listmedia = e.data.listmedia;
